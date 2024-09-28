b73233e2ab7ce101eb374141ab9c7c6a44b4256e
aparici0 <heyaparicio@gmail.com>
2024-09-28 09:51:14 -0300

    28 de sept
    CSS y html en página registro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -48,8 +48,8 @@
                         </div>
         
                         <div class="row">
-                            <div class="col-md-6">
-                                <label for="password" class="form-label">{{ __('Password') }}</label>
+                            <div class="col-md-12">
+                                <label for="password" class="form-label">{{ __('Contraseña') }}</label>
 
                                 <div>
                                     <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
@@ -59,10 +59,18 @@
                                             <strong>{{ $message }}</strong>
                                         </span>
                                     @enderror
+                                    <p class="txt-small">
+                                        <svg width="13" height="15" viewBox="0 0 13 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M11.0035 6.61865H1.90352C1.18555 6.61865 0.603516 7.20068 0.603516 7.91865V12.4687C0.603516 13.1866 1.18555 13.7687 1.90352 13.7687H11.0035C11.7215 13.7687 12.3035 13.1866 12.3035 12.4687V7.91865C12.3035 7.20068 11.7215 6.61865 11.0035 6.61865Z" fill="#747E67" stroke="#747E67" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M3.20361 6.61855V4.01855C3.20361 3.1566 3.54602 2.32995 4.15552 1.72046C4.76501 1.11096 5.59166 0.768555 6.45361 0.768555C7.31557 0.768555 8.14222 1.11096 8.75171 1.72046C9.3612 2.32995 9.70361 3.1566 9.70361 4.01855V6.61855" stroke="#747E67" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
+                                        </svg>
+                                        <span>Contraseña de al menos 8 caracteres</span>
+                                    </p>
                                 </div>
+                                
                             </div>
-                            <div class="col-md-6">
-                                <label for="password-confirm" class="form-label">{{ __('Confirmar Password') }}</label>
+                            <div class="col-md-12">
+                                <label for="password-confirm" class="form-label">{{ __('Confirmar Contraseña') }}</label>
 
                                 <div>
                                     <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
@@ -72,7 +80,7 @@
                         <div>
                             <div class="form-check">
                                 <input class="form-check-input" type="checkbox" value="1" id="conditions" required>
-                                <label class="form-check-label" for="conditions">
+                                <label class="form-check-label mt-24" for="conditions">
                                     Acepto los <a class="enlace" href="#">términos y condiciones y políticas de privacidad</a>
                                 </label>
                             </div>
